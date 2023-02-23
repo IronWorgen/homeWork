323ef33bdaca0b98f72cabd1ebf90a8abdeed749
ivasenko <ivasenko98@list.ru>
2023-02-23 23:57:23 +0300

    algorithm rework + console Input add

--- HW_sem1/mission2/Program.cs
+++ HW_sem1/mission2/Program.cs
@@ -1,18 +1,16 @@
 ﻿// максимальное из 3-х чисел
-int a = 2, b= 7, c=3;
-int max = a; 
-if (a > b)
-{
-    if (a>c){
-        Console.WriteLine(a);
-    }else {
-        Console.WriteLine(c);
-    }
-}else {
-    if (b>c){
-        Console.WriteLine(b); 
-    }else {
-        Console.WriteLine(c);
-    }
-}
+
+Console.WriteLine("Введите чило №1");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите чило №2");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите чило №3");
+int number3 = Convert.ToInt32 (Console.ReadLine());
+
+int max = number1;
+
+if (number2>max) max = number2;
+if (number3>max) max = number3;
+
+Console.WriteLine(max);
 

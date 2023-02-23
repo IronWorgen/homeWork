72f34989feb50e7e0a4eec985dd620f02d8e8f41
ivasenko <ivasenko98@list.ru>
2023-02-23 19:26:53 +0300

    ready mission1

--- HW_sem1/mission1/Program.cs
+++ HW_sem1/mission1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int  a = -9, b = -3;
+if (a>b){
+    Console.WriteLine(a);
+}
+else {
+    Console.WriteLine(b);
+}

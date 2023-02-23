9378d237d9ca7ea1a31cf221111ac7552bee1db0
ivasenko <ivasenko98@list.ru>
2023-02-23 19:34:31 +0300

    mission 2 ready

--- HW_sem1/mission2/Program.cs
+++ HW_sem1/mission2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// максимальное из 3-х чисел
+int a = 2, b= 7, c=3;
+int max = a; 
+if (a > b)
+{
+    if (a>c){
+        Console.WriteLine(a);
+    }else {
+        Console.WriteLine(c);
+    }
+}else {
+    if (b>c){
+        Console.WriteLine(b); 
+    }else {
+        Console.WriteLine(c);
+    }
+}
+

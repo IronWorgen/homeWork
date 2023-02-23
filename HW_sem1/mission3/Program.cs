7bc98f4b71b66154d42b036b7f8c06c618519887
ivasenko <ivasenko98@list.ru>
2023-02-23 23:27:01 +0300

    mission3 complite

--- HW_sem1/mission3/Program.cs
+++ HW_sem1/mission3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите чило");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 ==0)
+{
+    Console.WriteLine("да");
+}
+else Console.WriteLine("нет");

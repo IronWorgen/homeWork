c1a8405cbc8b169317a62f67b3856d432d94894d
ivasenko <ivasenko98@list.ru>
2023-02-24 00:24:03 +0300

    mission4 v1.0

--- HW_sem1/mission4/Program.cs
+++ HW_sem1/mission4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i< n ;i++){
+    if (i%2==0){
+        Console.Write("{0}, ",i);
+    }
+    
+}

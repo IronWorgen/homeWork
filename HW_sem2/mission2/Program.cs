c6f5c4380b372a8087af1d460cbc7c3186af0e5a
ivasenko <ivasenko98@list.ru>
2023-02-27 19:57:54 +0300

    mission 2 complete

---
+++ HW_sem2/mission2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("введите число");
+string num = Console.ReadLine();
+
+if (num.Length==2)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else 
+{
+    Console.WriteLine(num[2]);
+}

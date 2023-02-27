0d41cc440d63672b318a9b79f50b91ab65b0d77a
ivasenko <ivasenko98@list.ru>
2023-02-27 19:46:43 +0300

    mission 1 complete

---
+++ HW_sem2/mission1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число");
+string num = Console.ReadLine();
+
+bool triger = true;
+
+while (triger) {
+
+    if (num.Length==3)
+    {
+        break;
+    }
+
+    Console.WriteLine("Введите трехзначное число");
+    num = Console.ReadLine();
+}
+
+Console.WriteLine(num[1]);

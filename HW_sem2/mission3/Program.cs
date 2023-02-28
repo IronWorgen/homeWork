663f2d7ae3218104c073d52b4ffda3f6a2bd0afc
ivasenko <ivasenko98@list.ru>
2023-02-28 13:59:36 +0300

    mission 3 complete

---
+++ HW_sem2/mission3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("введите число:");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 7 | num == 6 )
+{
+    Console.WriteLine ("да");
+}
+else
+{
+    if ( num < 6 & num > 0)
+    {
+        Console.WriteLine ("нет ");
+    }
+    else
+    {
+        Console.WriteLine ("нет такого дня недели");
+    }
+}
+
+

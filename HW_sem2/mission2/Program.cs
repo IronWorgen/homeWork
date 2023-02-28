a599e879d1c7c8cbdfe29947c81de0ffdd2e1b5f
ivasenko <ivasenko98@list.ru>
2023-02-28 13:51:22 +0300

    mission2 complete

--- HW_sem2/mission2/Program.cs
+++ HW_sem2/mission2/Program.cs
@@ -1,11 +1,42 @@
-﻿Console.WriteLine ("введите число");
-string num = Console.ReadLine();
+﻿//решение через строки 
 
-if (num.Length==2)
+//Console.WriteLine ("введите число");
+//string num = Console.ReadLine();
+//
+//if (num.Length==2)
+//{
+//    Console.WriteLine("третьей цифры нет");
+//}
+//else 
+//{
+//    Console.WriteLine(num[2]);
+//}
+
+//решение без использования строк
+
+Console.WriteLine ("введите число");
+int num = int.Parse(Console.ReadLine());
+int pos_3=0;
+int counter = 0;
+int delitel = 10;
+//нахожу длуну числа 
+while (num / delitel != 0) 
+{
+    counter ++;
+    delitel *=10;
+}
+
+if (counter < 2) 
 {
     Console.WriteLine("третьей цифры нет");
 }
-else 
+else
 {
-    Console.WriteLine(num[2]);
+    pos_3 = num / (delitel/1000);
+    pos_3 = pos_3 % 10;
+    Console.WriteLine (pos_3);
+    
 }
+
+
+

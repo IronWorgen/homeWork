18782cfb9c5057b34cd35df2ed0cf1114342dddf
ivasenko <ivasenko98@list.ru>
2023-03-03 00:25:19 +0300

    add string solution

--- HW_sem3/mission1/Program.cs
+++ HW_sem3/mission1/Program.cs
@@ -13,12 +13,13 @@ while ( num/delitel != 0 ){
     lenNum ++;
     delitel *=10;
 }
-
-if (lenNum == 1 ){
-    Console.WriteLine("да");
+// если длина = 1 или число отрацательное - оно не является палиндромом
+if (lenNum == 1 | num <0 ){
+    Console.WriteLine("нет");
     return;
 }
-// помещаем каждую цифру в массив равный длине чила num  
+
+// помещаем каждую цифру в массив равный длине числа num  
 int[] nums = new int [lenNum];
 int temp =  num;
 
@@ -28,7 +29,7 @@ for ( int i =0 ; i<lenNum; i ++ )
     temp = temp % Convert.ToInt32(Math.Pow(10, lenNum-1-i));
 }
 
-//поэлементно сравниваем значения в массиве первый с последним, второй с предпоследним...
+//поэлементно сравниваем значения в массиве (первый с последним, второй с предпоследним...)
 bool itsPalindrome = true;
 
 for ( int i =0 ; i<nums.Length/2; i++)
@@ -39,5 +40,25 @@ for ( int i =0 ; i<nums.Length/2; i++)
 if (itsPalindrome) Console.WriteLine( "да");
 else Console.WriteLine("нет");
 
+
+//решение через строки ------------------------------------------------------------
+
+Console.Write("Решение через строки ------------------------------\nВведите число: ");
+String number = Console.ReadLine();
+
+if (number.Length == 1 | number[0]=='-' ){
+    Console.WriteLine("нет");
+    return;
+}
+
+itsPalindrome = true;
+
+for ( int i =0 ; i<number.Length/2; i++)
+{
+    if (nums[i] != nums[number.Length-1-i]) {itsPalindrome = false;}
+}
+
+if (itsPalindrome) Console.WriteLine( "да");
+else Console.WriteLine("нет");
 
 

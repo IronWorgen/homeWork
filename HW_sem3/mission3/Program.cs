d2a6930e90a5a5f03ffac1b5a926c8a142792edd
ivasenko <ivasenko98@list.ru>
2023-03-03 00:05:07 +0300

    mission 3 complete

---
+++ HW_sem3/mission3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void getCubeTable (int number)
+{
+    for (int i = 1; i <= number; i ++){
+        if (i == number) 
+        {
+            Console.WriteLine(Math.Pow(i,3)+".");
+            break;
+
+        }
+        Console.Write(Math.Pow(i,3)+", ");
+    }
+}
+
+getCubeTable (5);

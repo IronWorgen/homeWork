2137983376469926f6db7e77072bad3f36363957
ivasenko <ivasenko98@list.ru>
2023-03-08 11:18:48 +0300

    mission 3 complete

---
+++ HW_sem4/Mission3/Mission3.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая задаёт массив и выводит его на экран.
+
+int[] arrPrint (int len){
+    
+    int[] arr =  new int[len];
+    Random rand = new Random();
+
+    for( int i =0 ; i < arr.Length; i++){
+        arr[i]= rand.Next(0,100);
+        //для пследнего элемента не печатаем запятую
+        if (i == arr.Length-1){
+            Console.Write(arr[i]);
+            break;
+            }
+        Console.Write(arr[i]+", ");
+    }
+    return arr;
+}
+
+arrPrint(10);

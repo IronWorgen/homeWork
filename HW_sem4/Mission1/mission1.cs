e3e8f70dbddc9435ffbc8ba94556f2d4d698cf38
ivasenko <ivasenko98@list.ru>
2023-03-08 10:53:30 +0300

    Mission 1 complete

---
+++ HW_sem4/Mission1/mission1.cs
@@ -0,0 +1,14 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int aPowB (int a, int b ){
+    int result =a;    
+    if (b == 1) return a;
+    if (b == 0) return 1;
+    for (int i =2; i <= b; i++){
+        result *=a ;
+    }
+    return result;
+}
+
+
+Console.WriteLine(aPowB (3,1));

049a458bf031b11e1d575fa497b162f8d44b618e
ivasenko <ivasenko98@list.ru>
2023-03-08 11:03:12 +0300

    Mission 2 complete

---
+++ HW_sem4/Mission2/Mission2.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sumNum (int num){
+    int newNum = num;
+    int result = 0;
+
+    if (newNum<0){newNum=-newNum;}
+
+    while ( newNum != 0 ){
+        result += newNum%10;
+        newNum = newNum/10;
+    }
+    
+    return result;
+}
+
+Console.WriteLine(sumNum(-9012));

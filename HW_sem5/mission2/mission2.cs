d743539af5c95856190b55e1976cb2b061fc140f
ivasenko <ivasenko98@list.ru>
2023-03-10 19:04:08 +0300

    mission2 complete

---
+++ HW_sem5/mission2/mission2.cs
@@ -0,0 +1,18 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int sum(int len){
+    int sum  =0;
+    Random rand = new Random();
+    int [] arr = new int[len];
+
+    for (int i = 0; i< len; i++){
+        arr [i] = rand.Next(0,10);
+        if (i % 2 != 0){sum+=arr[i];}
+        
+    }
+
+    return sum;
+}
+
+Console.Write(sum(5));

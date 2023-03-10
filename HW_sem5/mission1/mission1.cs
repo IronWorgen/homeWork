382efb0e4d4e35614b92d54e8079f8a319469e26
ivasenko <ivasenko98@list.ru>
2023-03-10 18:50:31 +0300

    mission 1 complete

---
+++ HW_sem5/mission1/mission1.cs
@@ -0,0 +1,20 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int sumEven(int len){
+    int counter  =0;
+    Random rand = new Random();
+    int [] arr = new int[len];
+
+    for (int i = 0; i < len; i++){
+        arr[i] = rand.Next(100,1000);
+        if (arr[i]%2==0){ counter ++; }
+        
+    }
+
+    return counter;
+
+}
+
+Console.WriteLine(sumEven(10));
+

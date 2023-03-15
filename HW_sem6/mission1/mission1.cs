860b6b5ed6522890faa881b8d5b762acb9ec6346
ivasenko <ivasenko98@list.ru>
2023-03-15 14:32:57 +0300

    mission1 complete

---
+++ HW_sem6/mission1/mission1.cs
@@ -0,0 +1,14 @@
+﻿int countMoreNull(int m){
+    int counter =0;
+    for ( int i = 0 ; i <= m; i ++){
+        Console.Write("Введите число №"+ i + ": ");
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0){
+            counter++;
+        }
+        Console.WriteLine();
+    }
+    return counter;
+}
+
+Console.Write(countMoreNull(5));
